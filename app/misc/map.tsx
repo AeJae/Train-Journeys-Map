fe50f8b4a1f6059d99cc7c4b56f5735a89b4d0b1
Arun Fletcher <focalpear00@yahoo.co.uk>
2024-09-04 00:21:00 +0100

    Stations are now put onto the map using the database

--- app/misc/map.tsx
+++ app/misc/map.tsx
@@ -8,27 +8,32 @@ import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet"
 import {icon, LatLng} from "leaflet";
 
 export default function Map({markers, links}: {markers: any, links: any}) {
-    const center = new LatLng(51.45, -0.155);
+    const london = new LatLng(51.45, -0.155);
 
     const stnIcon = icon({
         iconUrl: "stn.png",
-
         iconSize: [28, 28],
         iconAnchor: [14, 14],
         popupAnchor: [0, -10]
     })
 
+    let markerList = []
+    for (const i in markers) {
+        const m = markers[i];
+        markerList.push(
+            <Marker key={"m"+i} position={new LatLng(parseFloat(m.lat), parseFloat(m.long))} icon={stnIcon}>
+                <Popup>{m.name}</Popup>
+            </Marker>
+        )
+    }
+
     return (
-        <MapContainer className={"h-[calc(100dvh-4rem)]"} center={center} zoom={5}>
+        <MapContainer className={"h-[calc(100dvh-4rem)]"} center={london} zoom={6}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={new LatLng(51.5280408, -0.1333271)} icon={stnIcon}>
-                <Popup>
-                    London Euston
-                </Popup>
-            </Marker>
+            {markerList}
         </MapContainer>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,18 @@ import dynamic from "next/dynamic";
 
 export default async function Home() {
     const Map = useMemo(() => dynamic(() => import('@/app/misc/map'), {
-        loading: () => <p>A map is loading</p>,
+        loading: () => <p>Loading map...</p>,
         ssr: false
     }), [])
 
+    const rawMarkers = await fetch("http://localhost:3000/api/stations", {cache: "no-cache"});
+    const markersJson = await rawMarkers.json();
+    const rawLinks = await fetch("http://localhost:3000/api/links", {cache: "no-cache"});
+    const linksJson = await rawLinks.json();
+
     return (
         <div>
-            <Map markers={null} links={null}/>
+            <Map markers={markersJson} links={linksJson}/>
         </div>
     )
 }
