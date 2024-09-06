73c7f7f176600ec8c08a7e36cb28e07d6146dd12
Arun Fletcher <focalpear00@yahoo.co.uk>
2024-09-06 20:57:31 +0100

    Finish links page frontend aside from error/success messages

--- app/links/newLink.tsx
+++ app/links/newLink.tsx
@@ -1,3 +1,63 @@
-export default function NewLink() {
+'use client'
 
+import {useState} from "react";
+
+export default function NewLink({addLink}: {addLink: Function}) {
+    const [showCreate, setShowCreate] = useState(false)
+    const [locAText, setLocAText] = useState("");
+    const [locBText, setLocBText] = useState("");
+
+    function loc_a_handler(e: any) {
+        setLocAText(e.target.value);
+    }
+
+    function loc_b_handler(e: any) {
+        setLocBText(e.target.value);
+    }
+
+    function handleCancel(e: any) {
+        e.preventDefault();
+        setShowCreate(false);
+    }
+
+    function handleSubmit(e: any) {
+        e.preventDefault();
+        // Convert to title case...
+        const locA = locAText.split(' ').map(w => w[0].toUpperCase() + w.substring(1).toLowerCase()).join(' ');
+        const locB = locBText.split(' ').map(w => w[0].toUpperCase() + w.substring(1).toLowerCase()).join(' ');
+        // Process on server...
+        addLink(locA, locB);
+    }
+
+    return (
+        <>
+        {/* Normal tab shown with no interaction */}
+        {!showCreate &&
+            <button onClick={() => {setShowCreate(true)}} className={"flex items-center justify-between p-2 " +
+                "bg-slate-300 w-full mb-2 rounded-md hover:outline hover:outline-1 hover:outline-slate-400"}>
+                <p className={"font-bold"}>Create a new link</p>
+                <div className={"flex justify-center items-center h-16 sm:h-fit"}>
+                    <p className={"py-1 px-2 w-20 bg-green-500 rounded-md"}>Create</p>
+                </div>
+            </button>
+        }
+
+        {/* Tab shown after user has decided to add a new link */}
+        {showCreate && (
+            <div className={"p-2 bg-slate-300 w-full mb-2 rounded-md hover:outline hover:outline-1 hover:outline-slate-400"}>
+                <form className={"flex justify-between items-center"}>
+                    <div className={"flex flex-col sm:flex-row items-center"}>
+                        <input type={"text"} placeholder={"Location A"} className={"py-1 px-2 rounded-sm outline-none"} onChange={loc_a_handler} value={locAText} />
+                        <p className={"m-0 sm:mx-1.5 h-0 sm:h-fit"}>{"<-->"}</p>
+                        <input type={"text"} placeholder={"Location B"} className={"py-1 px-2 rounded-sm outline-none"} onChange={loc_b_handler} value={locBText} />
+                    </div>
+                    <div className={"flex flex-col ml-1 sm:flex-row justify-center items-center h-16 sm:h-fit"}>
+                        <button className={"p-1 w-20 bg-red-600 rounded-md text-white"} onClick={handleSubmit}>Confirm</button>
+                        <button className={"p-1 w-20 bg-green-500 rounded-md m-0 sm:ml-1"} onClick={handleCancel}>Cancel</button>
+                    </div>
+                </form>
+            </div>
+        )}
+        </>
+    )
 }
--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,13 +1,22 @@
 import Link from "@/app/links/link";
+import NewLink from "@/app/links/newLink";
 
 export default async function Page() {
     const rawLinks = await fetch("http://localhost:3000/api/links", {cache: "no-cache"});
     const links = await rawLinks.json();
 
+    // Delete link from database
     async function deleteLink(link: any) {
         'use server'
         console.log("Delete:", link.location_a, "<->", link.location_b);
         // Actually delete it...
+    }
+
+    // Add link to database
+    async function addLink(locA: string, locB: string) {
+        'use server'
+        console.log(`Add: ${locA} <-> ${locB}`);
+        // Actually add it...
     }
 
     let linkElems = [];
@@ -18,6 +27,7 @@ export default async function Page() {
 
     return (
         <div className={"flex flex-col items-center m-4 font-medium"}>
+            <NewLink addLink={addLink}/>
             {linkElems}
             <p className={"text-slate-400 -mb-2"}>End of list.</p>
         </div>
